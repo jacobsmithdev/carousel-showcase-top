c58bc589d7afc7801ad0e34bfbc5dc6648acf91d
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-08-26 10:32:56 -0600

    fix: Add missing width declaration to slidesWrapper

--- src/Classes/Carousel.js
+++ src/Classes/Carousel.js
@@ -19,6 +19,7 @@ export default class Carousel {
         this.slidesWrapper.style.zIndex = '-1';
 
         // Ensure wrapper width matches its container
+        this.slidesWrapper.style.width = '100%';
         this.slidesWrapper.style.height = '100%';
 
         this.slidesWrapper.style.display = 'grid';
