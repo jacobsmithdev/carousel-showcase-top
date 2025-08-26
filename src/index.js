3a46cf5c8731b927af80973aedfea9a9dff714d9
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-08-26 10:01:54 -0600

    feat: Add mockup carousel for testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './styles.css';
 
-console.log('hello world');
+import Carousel from './Classes/Carousel';
+
+const container = document.querySelector('.carousel');
+const slidesArr = Array.from(
+    document.querySelector('.carousel__slides').children
+);
+const nextBtn = document.querySelector('.carousel__next');
+const prevBtn = document.querySelector('.carousel__prev');
+
+const carousel = new Carousel(container, slidesArr);
+
+nextBtn.addEventListener('click', () => carousel.next());
+prevBtn.addEventListener('click', () => carousel.prev());
+
+document.body.append(carousel.element);
