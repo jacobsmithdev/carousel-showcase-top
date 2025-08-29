39f38282351e22c5fb16c1e349e3d00ce2cfdbb7
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-08-29 08:38:25 -0600

    feat: Add mockup for carousel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './styles.css';
+
+import Carousel from './Classes/Carousel.js';
+
+const carouselContainer = document.querySelector('.carousel');
+const carouselDots = document.querySelector('.carousel__dots');
+const carouselSlides = Array.from(document.querySelector('.images').children);
+const nextBtn = document.querySelector('.carousel__next');
+const prevBtn = document.querySelector('.carousel__prev');
+
+const dot = document.createElement('div');
+dot.classList.add('dot');
+
+const dotCircle = document.createElement('div');
+dotCircle.classList.add('dot__circle');
+
+dot.append(dotCircle);
+
+const activeDot = dot.cloneNode(true);
+activeDot.classList.add('dot--active');
+
+const carousel = new Carousel(
+    carouselContainer,
+    carouselSlides,
+    carouselDots,
+    dot,
+    activeDot
+);
+
+nextBtn.addEventListener('click', () => carousel.next());
+prevBtn.addEventListener('click', () => carousel.prev());
